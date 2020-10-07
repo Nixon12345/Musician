e9b8f7da928c9fbe861013300c3309634b375f6d
rajdeep-4321 <rajdeep.raj.712@gmail.com>
2020-10-07 11:19:01 +0530

    Initial commit

---
+++ app/src/main/java/com/naeemdev/realtimechatwithfirebase/model/Event_DataModel.java
@@ -0,0 +1,10 @@
+package com.naeemdev.realtimechatwithfirebase.model;
+
+public class Event_DataModel {
+
+    public final String message;
+
+    public Event_DataModel(String message) {
+        this.message = message;
+    }
+}
